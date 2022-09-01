264cc4bef66452fb887ecfee9ee81766f3304cb3
PBardy <58274545+PBardy@users.noreply.github.com>
2022-09-01 15:58:32 +0100

    Improve email service

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -1,34 +1,15 @@
-import Email from 'email-templates';
 import { User } from '@/models/user.model';
 import { BaseService } from './base.service';
 import { MAIL_FROM, MAIL_HOST, MAIL_PASS, MAIL_PORT, MAIL_USER } from '@config';
 import { ForgotPasswordEmailDto } from '@/dtos/auth/ForgotPasswordEmail.dto';
+import path from 'path';
 
+const Email = require('email-templates');
 const NodeMailer = require('nodemailer');
 
 export class EmailService extends BaseService {
-  private getTransport() {
-    return NodeMailer.createTransport({
-      host: MAIL_HOST,
-      port: MAIL_PORT,
-      auth: {
-        user: MAIL_USER,
-        pass: MAIL_PASS,
-      },
-    });
-  }
-
-  public async sendForgotPasswordEmail(dto: ForgotPasswordEmailDto) {
-    /*
-    await this.getTransport().sendMail({
-      from: MAIL_FROM,
-      to: dto.user.email,
-      subject: 'Test Nodemailer with Mailtrap',
-      html: '<h1>Attachments</h1>',
-    });
-    */
-
-    const email = new Email({
+  private getEmail() {
+    return new Email({
       message: {
         from: MAIL_FROM,
       },
@@ -44,14 +25,17 @@ export class EmailService extends BaseService {
         },
       },
     });
+  }
 
-    await email.send({
-      template: 'forgot-password',
+  public async sendForgotPasswordEmail(dto: ForgotPasswordEmailDto) {
+    await this.getEmail().send({
+      template: path.join(__dirname, '../emails', 'forgot-password'),
       message: {
         to: dto.user.email,
       },
       locals: {
         name: dto.user.fullName,
+        recoveryCode: dto.recoveryCode,
       },
     });
   }
